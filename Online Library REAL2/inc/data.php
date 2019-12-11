a5bf23ea6eb91b1458b60b09e182ce7c07bf6d98
Elian <43243730+ElianSarmiento@users.noreply.github.com>
2019-12-11 18:29:36 -0500

    Add files via upload

---
+++ Online Library REAL2/inc/data.php
@@ -0,0 +1,165 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "The Seven Deadly Sins",
+	"img" => "img/media/7ds.png",
+    "genre" => "Fantasy",
+    "format" => "Paperback",
+    "year" => 2011,
+    "category" => "Books",
+    "authors" => [
+        "Nakaba Suzuki"
+    ],
+    "publisher" => "Kodansha Comic",
+    "isbn" => '9780374111199'
+];
+$catalog[102] = [
+    "title" => "Attack on Titan",
+    "img" => "img/media/AOT.png",
+    "genre" => "Fantasy",
+    "format" => "Paperback",
+    "year" => 2009,
+    "category" => "Books",
+    "authors" => [
+        "Hajime Isayama"
+    ],
+    "publisher" => "Kodansha comic",
+    "isbn" => '9780743273565'
+];
+$catalog[103] = [
+    "title" => "Bleach",
+    "img" => "img/media/bleach.png",
+    "genre" => "Adventure Fiction",
+    "format" => "Paperback",
+    "year" => 2001,
+    "category" => "Books",
+    "authors" => [
+        "Tite Kubo"
+    ],
+    "publisher" => "Shueisha",
+    "isbn" => '9781593082499'
+];
+$catalog[104] = [
+    "title" => "JoJo's Bizarre Adventure",
+    "img" => "img/media/jojo.png",
+    "genre" => "Adventure Fiction and Fantasy",
+    "format" => "Paperback",
+    "year" => 2001,
+    "category" => "Books",
+    "authors" => [
+        "Hirohiko Araki"
+    ],
+    "publisher" => "Shueisha",
+    "isbn" => '9781593081157'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Mission Impossible",
+    "img" => "img/media/Missionimpossible.png",
+    "genre" => "Action",
+    "format" => "Online Streaming",
+    "year" => 2011,
+    "category" => "Movies",
+    "director" => "Brad Bird",
+    "writers" => [
+        "Bruce Geller",
+        ],
+    "stars" => [
+			"Tom Cruise",
+			"Paula Patton",
+			"Jeremy Renner",
+    ]
+];
+$catalog[202] = [
+    "title" => "Lord Of The Rings Return Of The King",
+    "img" => "img/media/LordOfTheRings.png",
+    "genre" => "Fantasy",
+    "format" => "Online Streaming",
+    "year" => 2003,
+    "category" => "Movies",
+    "director" => "Peter Jackson",
+    "writers" => [
+        "J.R.R Tolkien",
+
+    ],
+    "stars" => [
+			"Sean Bean",
+			"Orlando Bloom",
+			"Ian McKellen"
+    ]
+];
+$catalog[203] = [
+    "title" => "Iron Giant",
+    "img" => "img/media/IronGiant.png",
+    "genre" => "Adventure",
+    "format" => "Online Streaming",
+    "year" => 1999,
+    "category" => "Movies",
+    "director" => "Brad Bird",
+    "writers" => [
+        "Tim McCanlies",
+    ],
+    "stars" => [
+			"Jennifer Aniston",
+			"Vin Diesel",
+			"Harry Connick Jr."
+    ]
+];
+$catalog[204] = [
+    "title" => "Terminator 2",
+		"img" => "img/media/Terminator2.png",
+	  "genre" => "Action,Science Fiction",
+	  "format" => "Online Streaming",
+	  "year" => 1991,
+	  "category" => "Movies",
+	  "director" => "James Cameron",
+	  "writers" => [
+	      "William Wisher"
+
+	  ],
+	  "stars" => [
+			"Arnold Schwarzenegger",
+			"Linda Hamilton",
+			"Edward Furlong"
+	  ]
+	];
+//Music
+$catalog[301] = [
+	"title" => "Billy Joel: An Innocent Man",
+	"img" => "img/media/BillyJoel.png",
+	"genre" => "Rock",
+	"format" => "CD",
+	"year" => 1983,
+	"category" => "Music",
+	"artist" => "Billy Joel"
+];
+
+$catalog[302] = [
+	"title" => "Linkin Park: In the End",
+	"img" => "img/media/LinkinPark.png",
+	"genre" => "Rap Rock",
+	"format" => "CD",
+	"year" => 2003,
+	"category" => "Music",
+	"artist" => "Linkin Park"
+];
+$catalog[303] = [
+	"title" => "Living in the Shadows",
+	"img" => "img/media/livingInTheShadows.png",
+	"genre" => "Folk",
+	"format" => "CD",
+	"year" => 2016,
+	"category" => "Music",
+	"artist" => "Matthew Perryman Jones"
+];
+$catalog[304] = [
+	"title" => "Relax",
+	"img" => "img/media/Relax.png",
+	"genre" => "Pop",
+	"format" => "CD",
+	"year" => 1984,
+	"category" => "Music",
+	"artist" => "Frankie Goes To Hollywood"
+];
+?>
